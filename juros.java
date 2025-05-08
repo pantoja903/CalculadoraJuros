3a4dbc327dffaaf417733c31b4c1f1c9dc04afa2
pantoja903 <arthur.pantoja.nery27@gmail.com>
2025-05-08 10:18:15 -0300

    commit calculadora de juros

---
+++ juros.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class juros {
+
+    public static void main(String[] args) {
+        //Declaração de variáveis 
+        double Juros, Capital, Taxa, Montante; 
+        int Prazo; 
+    
+    
+        //Criação e instância do objeto de entrada 
+        Scanner entrada = new Scanner(System.in);
+
+        //Apresentação 
+        System.out.println("\n\t\t\t -- Calculadora Juros --\n");
+
+        //Entrada
+    }
+}
